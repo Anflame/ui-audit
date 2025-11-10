cc1037890f0742ed28ba562ce7d7624daf18bbfd
sidorovdv <sidorovdv@gnivc.ru>
2025-11-10 14:30:00 +0400

    add deep using

--- src/components/Tree1Deep/FistDeep/OrganizationCard/Form/Form.tsx
+++ src/components/Tree1Deep/FistDeep/OrganizationCard/Form/Form.tsx
@@ -1,21 +1,19 @@
-import { Input } from "antd";
+import { Input, Form as AntdForm } from "antd";
 import { TestAntWrapped } from "@common/TestAntWrapped";
-// import { Checkbox } from "@common/CheckBox/CheckBox.tsx";
 import { Table } from "ksnm-common-ui-table";
 import { getColumns } from "./utils/getColumns.tsx";
 
 const Form = () => {
   return (
-    <form>
+    <AntdForm>
       <Input
         type="text"
         title="testTree1Deep.FirstDeep.OrganizationCard.Form.antd"
       />
       <TestAntWrapped title="testTree1Deep.FirstDeep.OrganizationCard.Form.antd-wrapped" />
       <input title="testTree1Deep.FirstDeep.OrganizationCard.Form.local" />
-      {/*<Checkbox />*/}
       <Table resource="" columns={getColumns()} />
-    </form>
+    </AntdForm>
   );
 };
 
